0e6ecbc299c04087445389acaeb52c75883f1692
datnt <datnthe140692@fpt.edu.vn>
2021-05-06 18:36:22 +0700

    Add Get Project

---
+++ vuejs-fetch-data/src/App.vue
@@ -0,0 +1,36 @@
+<template>
+  <div id="app">
+    <single-post v-for="post in posts" :key="post.id" :post="post"></single-post>
+  </div>
+</template>
+
+<script>
+import SinglePostVue from "./components/SinglePost.vue";
+export default {
+  name: "app",
+  components: {
+    "single-post": SinglePostVue,
+  },
+  data() {
+    return {
+      posts: [],
+    };
+  },
+  created() {
+    fetch("https://jsonplaceholder.typicode.com/users/1/posts")
+      .then((response) => response.json())
+      .then((posts) => this.posts = posts.slice(0, 5));
+  },
+};
+</script>
+
+<style lang="scss">
+#app {
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  color: #2c3e50;
+  max-width: 600px;
+  margin: 0 auto;
+}
+</style>
---
+++ vuejs-fetch-data/src/components/SinglePost.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="single-post">
+    <h3>{{ post.title }}</h3>
+    <p>User: {{ post.userId }} - ID: {{ post.id }}</p>
+    <p>{{ post.body }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ["post"],
+};
+</script>
+
+<style scoped>
+.single-post {
+    border: 1px solid #ddd;
+    padding: 10px;
+    margin: 10px 0;
+}
+</style>
