2ddb5dc1ac86e094388a6cdd359e4860d0f7ed00
datnt <datnthe140692@fpt.edu.vn>
2021-05-07 09:18:56 +0700

    add directives & filters

--- vuejs-fetch-data/src/App.vue
+++ vuejs-fetch-data/src/App.vue
@@ -29,6 +29,18 @@ export default {
         return post.title.match(this.search);
       })
     }
+  },
+  filters: {
+    toUppercase: function(value) {
+      return value.toUpperCase();
+    }
+  },
+  directives: {
+    'rainbow': {
+      bind(el, binding, vnode) {
+        el.style.color= '#' + Math.random().toString(16).slice(2, 8);
+      }
+    }
   }
 };
 </script>
--- vuejs-fetch-data/src/components/SinglePost.vue
+++ vuejs-fetch-data/src/components/SinglePost.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="single-post">
-    <h3>{{ post.title }}</h3>
+    <h3 v-rainbow>{{ post.title | toUppercase }}</h3>
     <p>User: {{ post.userId }} - ID: {{ post.id }}</p>
     <p>{{ post.body }}</p>
   </div>
@@ -9,13 +9,25 @@
 <script>
 export default {
   props: ["post"],
+  filters: {
+    toUppercase: function (value) {
+      return value.toUpperCase();
+    },
+  },
+  directives: {
+    rainbow: {
+      bind(el, binding, vnode) {
+        el.style.color = "#" + Math.random().toString(16).slice(2, 8);
+      },
+    },
+  },
 };
 </script>
 
 <style scoped>
 .single-post {
-    border: 1px solid #ddd;
-    padding: 10px;
-    margin: 10px 0;
+  border: 1px solid #ddd;
+  padding: 10px;
+  margin: 10px 0;
 }
 </style>
